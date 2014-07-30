0aa180b04cfb85fb8e2e9011867be20bfbb45ec8
Paul Swail <paulswail@gmail.com>
2014-07-30 16:41:11 +0100

    temporarily commented out api network calls so that empty VisitorData objects can be returned

--- dotnet/AutoChart.Sdk/VisitorService.cs
+++ dotnet/AutoChart.Sdk/VisitorService.cs
@@ -38,22 +38,26 @@ namespace AutoChart.Sdk
             {
                 throw new ArgumentException("visitorId must be specified");
             }
-            var url = string.Format("{0}/visitors/{1}/summary", _apiRootUrl, visitorId);
-            using (var client = new WebClient())
-            {                
-                client.Headers.Add ("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
-                var data = client.OpenRead(url);
-                var reader = new StreamReader(data);
-                var json = reader.ReadToEnd();
-                //TODO: complete implementation of this by mapping JSON into VisitorSummary object.
-            }
-            return null;
+            //var url = string.Format("{0}/visitors/{1}/summary", _apiRootUrl, visitorId);
+            //using (var client = new WebClient())
+            //{                
+            //    client.Headers.Add ("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+            //    var data = client.OpenRead(url);
+            //    var reader = new StreamReader(data);
+            //    var json = reader.ReadToEnd();
+            //    //TODO: complete implementation of this by mapping JSON into VisitorSummary object.
+            //}
+            return new VisitorSummary();
         }
 
         public VisitorSummary GetVisitorSummaryByEmail(string email)
         {
+            if (string.IsNullOrEmpty(email))
+            {
+                throw new ArgumentException("email must be specified");
+            }
             //TODO: implement
-            throw new NotImplementedException();
+            return new VisitorSummary();
         }
     }
 }
